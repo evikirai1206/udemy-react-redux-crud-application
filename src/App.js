dad7c6511a2520883f4b4fca58995abd06355531
evikirai1206 <ami_koizumi9612@yahoo.co.jp>
2021-06-29 20:04:03 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React,{ Component } from 'react';
 
-class App extends Component {
-  render(){
+//class App extends Component {
+//  render(){
+//
+//    return (
+//      <div>
+//        <label htmlFor="bar">bar</label>
+//        <input type="text" onChange={() =>{console.log("I am clicked.")}} />
+//      </div>
+//    )
+//  }
+//}
 
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() =>{console.log("I am clicked.")}} />
-      </div>
-    )
-  }
+const App = () =>{
+  return( 
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+
+const Cat = () =>{
+  return <div>Meow!</div>
+}
+
 
 export default App;
