bb1f482bbfc1de9aa64c73d3430ed9a001d2e753
evikirai1206 <ami_koizumi9612@yahoo.co.jp>
2021-06-29 18:43:38 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hello-world</h1>
-	<p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() =>{console.log("I am clicked.")}} />
+      </div>
+    )
+  }
 }
 
 export default App;
